0cfb8958a28d27b4ae28272e3034b4fd6918cbaf
Nick Coats <coats.18@gmail.com>
2018-02-17 22:02:20 -0500

    add tood

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ruby-quickstart
+
+## references
+
+* https://github.com/machzqcq/page-object_framework
+* 
+
+## TODO
+
+* cucumber
+* api
+* data
+* mobile
+* data
+* cli-ify
+* gemify
---
+++ spec/overall_spec.rb
@@ -0,0 +1,23 @@
+# require_relative("../src/hello.rb")
+
+RSpec.describe "overall" do
+
+    before do
+        
+    end
+
+    it "should run cucumber single node" do
+    end
+
+    it "should run cucumber on a grid" do
+    end
+
+    it "should run mobile" do
+    end
+
+    it "should run api" do
+    end
+
+    it "should run data" do
+    end
+end
